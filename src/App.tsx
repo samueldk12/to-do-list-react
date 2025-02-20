b1c3581eef485e8ffb0038e28727f6ee7e6e769c
Samuel Arao <samuel.arao@gmail.com>
2025-02-20 19:16:29 -0300

    feat: finalizacao react

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,82 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Header } from './components/Header'
 import { Task } from './components/Task'
-import { TaskBar } from './components/TaskBar'
+import { PlusCircle } from 'phosphor-react'
+import { v4 as uuid } from 'uuid'
 
 import app from './App.module.css'
 
 import './global.css'
 
+interface Task{
+  isFinished: boolean,
+  content: string,
+  id: string
+}
+
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [newTaskText, setNewTaskText] = useState('')
+  const [tasks, setTasks] = useState<Task[]>([])
+  const completedTasks = tasks.filter(task => task.isFinished).length;
+
+  function handleChangeTaskText(event: ChangeEvent<HTMLTextAreaElement>){
+    setNewTaskText(event.target.value)
+  }
+
+  function handleCompleteTask(id: string, isCompleted: boolean){
+
+    setTasks(tasks.map(task => {
+        if(task.id == id){
+          task.isFinished = isCompleted
+        }
+        return task
+     }))
+  }
+
+  function handleDeleteTask(id: string){
+        const taskIdDeletedOne = tasks.filter(task => {
+            return id != task.id
+        })
+
+        setTasks(taskIdDeletedOne)
+  }
+
+  function handleCreateTask(event: ChangeEvent<FormDataEvent>){
+    event.preventDefault()
+    const newTask: Task = { 
+      content: newTaskText,
+      isFinished: false,
+      id: uuid()
+    }
+    setTasks((prevTasks) => [...prevTasks, newTask])
+    setNewTaskText('')
+  }
 
   return (
     <>
       <Header/>
       <div className={app.taskBody}>
-        <TaskBar/>
+        <div className={app.taskBar}>
+            <form onSubmit={handleCreateTask} className={app.taskForm}>
+                <textarea onChange={handleChangeTaskText} value={newTaskText} className={app.taskValueInput} placeholder='Adicione uma nova tarefa' />
+                <button  type="submit">Criar <PlusCircle/></button>
+            </form>
+        </div>
         <div className={app.textQuanti}>
-          <strong className={app.createdTextTasks}>Tarefas criadas <strong className={app.qtdCreatedTasks}>{tasks.length}</strong></strong>
-          <strong className={app.qtdTextFinishedTasks}>Concluídas <strong className={app.qtdCreatedTasks}>0 de {tasks.length}</strong></strong>
+          <strong className={app.createdTextTasks}>
+            Tarefas criadas <strong className={app.qtdCreatedTasks}>{tasks.length}</strong>
+          </strong>
+          <strong className={app.qtdTextFinishedTasks}>
+            Concluídas <strong className={app.qtdCreatedTasks}>{completedTasks} de {tasks.length}</strong>
+          </strong>
+        </div>
+        <div>
+          
         </div>
         <div className={app.tasks}>
-            {
-              tasks.map((task)=>{
-                return <Task/>
-              })
-            }
+            {tasks.map((task, index) => (
+              <Task id={task.id} onDeleteFunction={handleDeleteTask} onCompletedFunction={handleCompleteTask} key={index} content={task.content} isCompleted={task.isFinished} />
+            ))}
         </div>
       </div>
     </>
@@ -32,3 +84,4 @@ function App() {
 }
 
 export default App
+ 
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,9 +1,37 @@
+import { ChangeEvent, useState } from 'react';
 import task from './Task.module.css'
+import {Trash} from 'phosphor-react'
 
-export function Task(){
+interface TaskProps {
+    content: string;
+    isCompleted: boolean;
+    id: string;
+    onDeleteFunction: (id:string)=>void;
+    onCompletedFunction: (id: string, isFinished: boolean)=>void;
+}
+
+export function Task({content,id, onCompletedFunction, onDeleteFunction,isCompleted = false}: TaskProps){
+    const [isFinished, setIsFinished] = useState(true)
+
+    function handleOnDeleteButton(){
+        onDeleteFunction(id, )
+    }
+
+    function handleOnCompletedFunction(event: ChangeEvent<HTMLInputElement>){
+        console.log(event.target.value)
+        setIsFinished(!isFinished)
+        onCompletedFunction(id, isFinished)
+    }
     return (
         <>
-         <p>Oi!</p>
+            <div className={task.body}>
+                <label className={task.isFinished}>
+                    <input value={isFinished} onChange={handleOnCompletedFunction} type="checkbox" checked={isCompleted}/>
+                    <span ></span>
+                </label>
+                <p className={task.content}>{content}</p>
+                <button className={task.deleteButton} onClick={handleOnDeleteButton}><Trash className={task.trash}/></button>
+            </div>
         </>
     )
 }
